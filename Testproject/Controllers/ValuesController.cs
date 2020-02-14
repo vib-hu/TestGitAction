a1f783cef06cfd080c6453e600062224872a4c25
[vibhu.mishra] <[vibhu.mishra@talabat.com]>
2020-02-14 14:30:14 +0400

    test3

--- Testproject/Controllers/ValuesController.cs
+++ Testproject/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace Testproject.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1 };
+            return new string[] { "value };
         }
 
         // GET api/values/5
